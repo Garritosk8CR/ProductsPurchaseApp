e09b1473fd6c0be9a1a13600b22f995d6df1f22b
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-03-24 10:58:14 -0600

    init

---
+++ main.py
@@ -0,0 +1,21 @@
+from typing import Union
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from redis_om import get_redis_connection, HashModel
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+# Connect to Redis
+
+redis_conn = get_redis_connection(
+    host="redis-12894.c1.us-east1-2.gce.redns.redis-cloud.com",
+    port=12894,
+    password="oUDjGzd2h2ljd6FyVIMdyvTp4gE3celk",
+    decode_responses=True
+)
